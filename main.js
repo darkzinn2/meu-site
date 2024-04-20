af7b426dc6fecdff6254ee0a4414ae4a7fdd6e8a
Darkness <143465981+darkzinn2@users.noreply.github.com>
2024-04-20 16:32:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // Header shadow scroll
-const header = document.querySelector('header');
-const navHeight = header.offsetHeight;
+const header = document.querySelector('header')
+const navHeight = header.offsetHeight
 
 window.addEventListener('scroll', function() {
-   if(window.scrollY >= navHeight) {
-      header.classList.add('scroll');
-   } else {
-      header.classList.remove('scroll');
+   if(window.scrollY >= navHeight){
+      header.classList.add('scroll')
+   }else{
+      header.classList.remove('scroll')
    }
-});
-
+})
 // Menu hambuguer
-const menuBtn = document.querySelector('#menu');
-const navLinks = document.querySelector('#nav-links');
-menuBtn.addEventListener('click', menuToggle);
+const menuBtn = document.querySelector('#menu')
+const navLinks = document.querySelector('#nav-links')
+menuBtn.addEventListener('click', menuToggle)
 
-// TEXTO MUDANDO 
+//TEXTO MUDANDO 
+
 var TxtType = function(el, toRotate, period) {
    this.toRotate = toRotate;
    this.el = el;
@@ -31,9 +31,9 @@ TxtType.prototype.tick = function() {
    var fullTxt = this.toRotate[i];
 
    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+   this.txt = fullTxt.substring(0, this.txt.length - 1);
    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+   this.txt = fullTxt.substring(0, this.txt.length + 1);
    }
 
    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
@@ -41,32 +41,30 @@ TxtType.prototype.tick = function() {
    var that = this;
    var delta = 200 - Math.random() * 100;
 
-   if (this.isDeleting) { 
-      delta /= 2; 
-   }
+   if (this.isDeleting) { delta /= 2; }
 
    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
+   delta = this.period;
+   this.isDeleting = true;
    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
+   this.isDeleting = false;
+   this.loopNum++;
+   delta = 500;
    }
 
    setTimeout(function() {
-      that.tick();
+   that.tick();
    }, delta);
 };
 
 window.onload = function() {
    var elements = document.getElementsByClassName('typewrite');
    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
+       var toRotate = elements[i].getAttribute('data-type');
+       var period = elements[i].getAttribute('data-period');
+       if (toRotate) {
          new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
+       }
    }
    // INJECT CSS
    var css = document.createElement("style");
@@ -75,19 +73,20 @@ window.onload = function() {
    document.body.appendChild(css);
 };
 
-function menuToggle() {
-   menuBtn.classList.toggle('active');
-   menuBtn.classList.toggle('bx-x');
-   navLinks.classList.toggle('active');
-   header.classList.toggle('active');
+
+function menuToggle(){
+   menuBtn.classList.toggle('active')
+   menuBtn.classList.toggle('bx-x')
+   navLinks.classList.toggle('active')
+   header.classList.toggle('active')
 }
 
 // CV
-const cvbtn = document.querySelector('.cv');
-const cvLink = './assets/Wyllyam_Vieira_Curriculo.pdf';
-cvbtn.setAttribute("href", cvLink);
+const cvbtn = document.querySelector('.cv')
+const cvLink = './assets/Wyllyam_Vieira_Curriculo.pdf'
+cvbtn.setAttribute("href", cvLink)
 
-// Scroll smooth
+// scroll smooth
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
    anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -96,3 +95,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
    });
 });
+
